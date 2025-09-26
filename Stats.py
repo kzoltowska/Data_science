f4c4e7f9b298f0fa4fe2151bdd578e76b065dfe0
Katarzyna Marta Zoltowska <167528622+kzoltowska@users.noreply.github.com>
2025-09-26 16:23:07 +0100

    Create Stats.py

---
+++ Stats.py
@@ -0,0 +1,26 @@
+def get_residuals(adata, design: str):
+  """ This function takes adata with X containing the data to be processed
+  and design the design to be used for model fitting.
+  The function loops over genes (columns), fitting a model per gene and saves residuals to
+  a new dataframe that is then placed in a new layer of anndata object """
+    import statsmodels.formula.api as smf
+    import pandas as pd
+    import numpy as np
+    # Fix gene names (hyphens are not allowed in formulas)
+    adata.var_names = adata.var_names.str.replace('-', '_', regex=True)
+    # Convert X to DataFrame 
+    X_df = pd.DataFrame(adata.X, columns=adata.var_names, index=adata.obs_names)
+    # Combine expression data with design variables
+    data = pd.concat([X_df, adata.obs], axis=1)
+    # Pre-allocate array for residuals - it is faster with numpy
+    residuals_matrix = np.zeros((adata.n_obs, adata.n_vars))
+    # Iterate over genes and compute residuals
+    for n, gene in enumerate(adata.var_names):
+        formula = f"{gene} ~ {design}"
+        model = smf.ols(formula=formula, data=data).fit()
+        residuals_matrix[:, n] = model.resid.values
+    # Convert to DataFrame
+    residuals_df = pd.DataFrame(residuals_matrix, columns=adata.var_names, index=adata.obs_names)
+    # Save to adata in a sepearate layer to keep the row data as well
+    adata.layers["residuals"] = residuals_df
+    return adata
